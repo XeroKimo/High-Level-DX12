311e238f6b3d1ee5d02d903d1641b4866e35696f
Renzy Alarcon <38732789+XeroKimo@users.noreply.github.com>
2019-04-20 16:53:05 -0400

    Add files via upload

---
+++ AppFramework/Framework/Source/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <Windows.h>
+#include "Framework.h"
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
